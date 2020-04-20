1e3a577b681a8d36fab4f78ea9f7aee91def6036
Geena Smith <geena_smith@hotmail.com>
2020-04-20 16:43:25 -0700

    created algRunner and localDescriptors

---
+++ src/algRunner.py
@@ -0,0 +1,20 @@
+
+# ** I think we should have a single runner file to make things easier. We can refactor the data loader and evaluator so it fits together better.
+
+
+# call data loader
+
+# get extract keypoints and get local descriptors
+# - harris-laplace: https://docs.opencv.org/3.4/d4/d7d/tutorial_harris_detector.html
+# - SIFT: https://docs.opencv.org/master/da/df5/tutorial_py_sift_intro.html
+
+# index scheme/hash table
+
+# retrieve descriptors
+
+# spatial organization (Hough transform)
+# - hough: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+
+# voting mechanism
+
+# evaluation alg
