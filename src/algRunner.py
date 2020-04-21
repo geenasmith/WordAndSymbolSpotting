49c8c046114f27b8b989c9a8b50d8b8ab148435f
Geena Smith <geena_smith@hotmail.com>
2020-04-20 18:15:38 -0700

    refactored dataLoader and updated algRunner

--- src/algRunner.py
+++ src/algRunner.py
@@ -1,12 +1,42 @@
+# * this is a single runner file. I thought it would be easier to combine our work this way.
 
-# ** I think we should have a single runner file to make things easier. We can refactor the data loader and evaluator so it fits together better.
+
+import sys
+from dataLoader import load_data
+from evaluateAlgorithm import evaluate
+
+# check that the user has included enough arguments
+if len(sys.argv) < 2:
+    print("Usage:\n\tpython3 dataLoader.py path/to/data [optional: randomize]")
+    exit(0)
+
+dataPath = sys.argv[1]
+
+if len(sys.argv) > 2:
+    randomize = True
+else:
+    randomize = False
 
 
-# call data loader
+# --------------------------------------------
+# Load Data
+# --------------------------------------------
+print("Loading Images...")
+queryImages, testImages, trainImages, testDict, trainDict = load_data(dataPath,randomize)
+print("Read " + str(len(queryImages)) + " query images, " + str(len(trainImages)) + " training images, and " + str(len(testImages)) + " testing images\n")
 
-# get extract keypoints and get local descriptors
+
+# --------------------------------------------
+# Extract Keypoints --> Geena
 # - harris-laplace: https://docs.opencv.org/3.4/d4/d7d/tutorial_harris_detector.html
+# --------------------------------------------
+
+
+# --------------------------------------------
+# Compute Descriptors --> Geena
 # - SIFT: https://docs.opencv.org/master/da/df5/tutorial_py_sift_intro.html
+# --------------------------------------------
+
 
 # index scheme/hash table
 
